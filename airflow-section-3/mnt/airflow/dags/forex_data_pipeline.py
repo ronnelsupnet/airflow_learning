653a2782d0508991c7dd6598fcadc80e3f5b280d
Ronnel Supnet <Supnetronnel@gmail.com>
2024-05-25 12:48:11 +0000

    forex data pipeline

--- airflow-section-3/mnt/airflow/dags/forex_data_pipeline.py
+++ airflow-section-3/mnt/airflow/dags/forex_data_pipeline.py
@@ -10,8 +10,8 @@ from airflow.providers.slack.operators.slack_webhook import SlackWebhookOperator
 
 from datetime import datetime, timedelta
 import csv
+import json 
 import requests
-import json
 
 default_args = {
     "owner": "airflow",
@@ -21,7 +21,6 @@ default_args = {
     "retries": 1,
     "retry_delay": timedelta(minutes=5)
 }
-
 def download_rates():
     BASE_URL = "https://gist.githubusercontent.com/marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b/raw/"
     ENDPOINTS = {
@@ -40,15 +39,17 @@ def download_rates():
             with open('/opt/airflow/dags/files/forex_rates.json', 'a') as outfile:
                 json.dump(outdata, outfile)
                 outfile.write('\n')
-
+ 
 def _get_message() -> str:
     return "Hi from forex_data_pipeline"
 
-with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1), 
-    schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
 
+# task created
+with DAG("forex_data_pipeline", start_date=datetime(2024, 1, 1), 
+         schedule_interval="@daily", default_args=default_args, catchup=False) as dag:
+    
     is_forex_rates_available = HttpSensor(
-        task_id="is_forex_rates_available",
+        task_id="is_forex_rates_available", 
         http_conn_id="forex_api",
         endpoint="marclamberti/f45f872dea4dfd3eaa015a4a1af4b39b",
         response_check=lambda response: "rates" in response.text,
@@ -56,8 +57,8 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
         timeout=20
     )
 
-    is_forex_currencies_file_available = FileSensor(
-        task_id="is_forex_currencies_file_available",
+    is_file_avialble = FileSensor(
+        task_id='check_forex_file',
         fs_conn_id="forex_path",
         filepath="forex_currencies.csv",
         poke_interval=5,
@@ -66,7 +67,8 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
 
     downloading_rates = PythonOperator(
         task_id="downloading_rates",
-        python_callable=download_rates
+        python_callable=download_rates,
+        dag=dag,
     )
 
     saving_rates = BashOperator(
@@ -98,10 +100,10 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
     )
 
     forex_processing = SparkSubmitOperator(
-        task_id="forex_processing",
-        application="/opt/airflow/dags/scripts/forex_processing.py",
-        conn_id="spark_conn",
-        verbose=False
+            task_id="forex_processing",
+            application="/opt/airflow/dags/scripts/forex_processing.py",
+            conn_id="spark_conn",
+            verbose=False
     )
 
     send_email_notification = EmailOperator(
@@ -115,9 +117,10 @@ with DAG("forex_data_pipeline", start_date=datetime(2021, 1 ,1),
         task_id="send_slack_notification",
         http_conn_id="slack_conn",
         message=_get_message(),
-        channel="#monitoring"
+        channel="#airflow-monitoring"
     )
     
-    is_forex_rates_available >> is_forex_currencies_file_available >> downloading_rates >> saving_rates 
+
+    is_forex_rates_available >> is_file_avialble >> downloading_rates >> saving_rates 
     saving_rates >> creating_forex_rates_table >> forex_processing
     forex_processing >> send_email_notification >> send_slack_notification 
